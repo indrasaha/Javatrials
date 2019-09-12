24be862441688e20e9f6d6e333f2dd36974ce0b2
indrasaha <indra.saha@gmail.com>
2019-09-12 23:10:03 +0530

    First commit

---
+++ JavaPerformanceTemplate/src/com/niltech/performance/cpu/ConcurrencyTemplate.java
@@ -0,0 +1,10 @@
+package com.niltech.performance.cpu;
+
+public class ConcurrencyTemplate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaPerformanceTemplate/src/com/niltech/performance/memory/MemoryOptimizationTemplate.java
@@ -0,0 +1,10 @@
+package com.niltech.performance.memory;
+
+public class MemoryOptimizationTemplate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
